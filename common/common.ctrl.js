3440905f76d4caa3f7d567b76995639726bc5f7a
rfathy <ran.fathy@gmail.com>
2019-03-30 12:53:32 +0200

    add ng-src and {{}} to profile image

--- common/common.ctrl.js
+++ common/common.ctrl.js
@@ -3,6 +3,7 @@ var app = angular.module("ngTask");
 app.controller("headerCtrl", function($scope) {
     $scope.dateOut = new Date();
     $scope.name = "Jane Doe";
+    $scope.img = "prof.jpg";
 
     //collapsing sidebar
 	$(document).ready(function() {
